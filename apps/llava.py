4bb4adbdc0204a25860d42225df6155ade650508
kentatonix <techlax8@gmail.com>
2024-07-26 02:47:03 +0000

    teachable agent追加　永続ストレージパス修正

--- apps/llava.py
+++ apps/llava.py
@@ -6,12 +6,12 @@ llava_config_list = [
     {
         "model": "ollama/llava:7b-v1.6-mistral-q6_K",
         "api_key": "None",
-        "base_url": "http://llm:11434",
+        "base_url": "http://llm:4000",
     }
 ]
 
 rst = llava_call(
-    "What's the breed of this dog? file is /workspace/dog.jfif",
+    "What's the breed of this dog? file is <img /home/autogen/autogen/apps/images/dog.jfif>",
     llm_config={"config_list": llava_config_list, "temperature": 0},
 )
 
--- apps/teach.py
+++ apps/teach.py
@@ -1,8 +1,9 @@
-import json
 from pathlib import Path
+from pickle import FALSE
 
 import autogen
 from autogen import AssistantAgent, UserProxyAgent
+from autogen.agentchat.contrib.capabilities.teachability import Teachability
 
 llm_config = {
     "config_list": [
@@ -14,7 +15,6 @@ llm_config = {
     ],
     "cache_seed": None,  # Turns off caching, useful for testing different models
 }
-
 
 script_name = Path(__file__).stem
 work_dir = Path("coding/" + script_name)
@@ -33,15 +33,24 @@ with autogen.coding.DockerCommandLineCodeExecutor(
     timeout=600,
     auto_remove=False,
 ) as code_executor:
+
     assistant = AssistantAgent("assistant", llm_config=llm_config)
-    user_proxy = UserProxyAgent(
-        "user_proxy", human_input_mode="NEVER", code_execution_config={"executor": code_executor}
+
+    # Instantiate the Teachability capability. Its parameters are all optional.
+    teachability = Teachability(
+        verbosity=3,  # 0 for basic info, 1 to add memory operations, 2 for analyzer messages, 3 for memo lists.
+        reset_db=FALSE,
+        path_to_db_dir="./teach_db/teachability_db",
+        recall_threshold=1.5,  # Higher numbers allow more (but less relevant) memos to be recalled.
     )
 
-    # Start the chat
-    user_proxy.initiate_chat(
-        assistant,
-        message="Plot a chart of NVDA and TESLA stock price change YTD. Save the plot to a file called plot.png",
-    )
+    # Instantiate a UserProxyAgent to represent the user. But in this notebook, all user input will be simulated.
+    user = UserProxyAgent(name="user", human_input_mode="NEVER", code_execution_config={"executor": code_executor})
+
+    # Now add the Teachability capability to the agent.
+    teachability.add_to_agent(assistant)
+
+    text = "Plot a chart of NVDA and TESLA stock price change YTD. Save the plot to a file called plot.png"
+    user.initiate_chat(assistant, message=text, clear_history=True)
 
 autogen.runtime_logging.stop()
